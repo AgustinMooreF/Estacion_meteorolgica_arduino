9b4d93792ad461a85d86516c84a171244b0649a4
thekidfrankie <agustinmoore13@gmail.com>
2019-05-23 11:38:15 -0400

    agregado bluetooth

--- proyecto_AC.ino
+++ proyecto_AC.ino
@@ -5,7 +5,9 @@
 #include <Adafruit_BMP280.h>
 #include <DHT.h>
 #include <LiquidCrystal.h>
-#include <Servo.h>
+//#include <Servo.h>
+#include <SoftwareSerial.h>    
+
 
 
 #define DHTPIN 6 //Defiene el pin al que se conectará el sensor
@@ -19,14 +21,15 @@ int d5 = 4;
 int d6 = 3;
 int d7 = 2;
 
-//Servo servo1; // definir Servo
+Servo servo1; // definir Servo
 Adafruit_BMP280 bmp; // definir sensor barometrico i2c
 DHT dht(DHTPIN, DHTTYPE);//definir tipo de sensor dht
 LiquidCrystal lcd( RS, E, d4, d5, d6, d7); //configurar pantalla lcd
-
+SoftwareSerial BT(10,11); 
 
 void setup() {
   
+  BT.begin(9600);
   Serial.begin(9600);
   dht.begin();
   lcd.begin(16,2);
@@ -42,7 +45,8 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(7,HIGH);
+
+  
   int hum_dht = dht.readHumidity();
   int temp_dht = dht.readTemperature();
   int pres_bmp = bmp.readPressure();
@@ -68,14 +72,24 @@ void loop() {
   lcd.print("Hpa");
   
   delay(8000);
-  
+
+   if(BT.available())    
+  {
+    Serial.write(BT.read());
+  }
+ 
+  if(Serial.available())  
+  {
+     BT.write(Serial.read());
+  }  
+ 
   if(temp_dht > 22 && hum_dht < 40 && pres_bmp > 28000 ){
        digitalWrite(7,HIGH);
-      servo1.write(90);
+        servo1.write(90);
     }
        else{
-           digitalWrite(7,LOW);
-           servo1.write(0);
+          digitalWrite(7,LOW);
+          servo1.write(0);
        }
   
     
